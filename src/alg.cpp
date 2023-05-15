91a1aa7b565937c695353f795800ca80992721f4
Misha Filchagin <79020757+Defender-sec@users.noreply.github.com>
2023-05-15 20:57:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,18 +1,17 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int i;
-  int colv=0;
-  for (i=0; i<15; i++){
-    a=arr[i];
-    if( value = a){
-      colv++;
-      }
-  }
-  if( colv>0){
-    return colv;
-  }else{
-    return 0;
-  }
-  
+    int kolv = 0;
+    int i;
+    for (i = 0; i < 15; i++) {
+        if (arr[i] == value) {
+            kolv++;
+        }
+    }
+    if (kolv > 0) {
+        return kolv;
+    }
+    else {
+        return 0;
+    }
 }
