f1fe46fe5767eb3c1dd13a732493e0104f7ab2ea
Misha Filchagin <79020757+Defender-sec@users.noreply.github.com>
2023-05-15 21:03:45 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // Copyright 2022 NNTU-CS
 #include "alg.h"
-
+#include "iostream"
+using namespace std;
 int main() {
     int arr[] = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5};
     int count = cbinsearch(arr, 15, 3); // count = 3
